dec2c0534e7ecdfaf3618eabdcbe652386550932
wangshengli <wangshengli@gomeholdings.com>
2018-01-09 10:22:29 +0800

    fix:修改别名

--- education-framework-parent/education-framework-dao/src/main/java/com/education/framework/dao/teacher/impl/TeacherCertificateDaoImpl.java
+++ education-framework-parent/education-framework-dao/src/main/java/com/education/framework/dao/teacher/impl/TeacherCertificateDaoImpl.java
@@ -12,32 +12,32 @@ public class TeacherCertificateDaoImpl extends SqlSessionDaoSupport implements T
 
 	@Override
 	public List<TeacherCertificate> findAllTeacherCertificate(Map<String, Object> map) {
-		return this.getSqlSession().selectList("teacher.findAllTeacherCertificate", map);
+		return this.getSqlSession().selectList("teacherCertificate.findAllTeacherCertificate", map);
 	}
 
 	@Override
 	public int findTeacherCertificateCount(Map<String, Object> map) {
-		return this.getSqlSession().selectOne("teacher.findTeacherCertificateCount", map);
+		return this.getSqlSession().selectOne("teacherCertificate.findTeacherCertificateCount", map);
 	}
 
 	@Override
 	public int deleteTeacherCertificateById(String id) {
-		return this.getSqlSession().delete("teacher.deleteTeacherCertificateById", id);
+		return this.getSqlSession().delete("teacherCertificate.deleteTeacherCertificateById", id);
 	}
 
 	@Override
 	public int insertTeacherCertificate(TeacherCertificate teacherCertificate) {
-		return this.getSqlSession().insert("teacher.insertTeacherCertificate", teacherCertificate);
+		return this.getSqlSession().insert("teacherCertificate.insertTeacherCertificate", teacherCertificate);
 	}
 
 	@Override
 	public int updateTeacherCertificateById(TeacherCertificate teacherCertificate) {
-		return this.getSqlSession().update("teacher.updateStrudentById", teacherCertificate);
+		return this.getSqlSession().update("teacherCertificate.updateStrudentById", teacherCertificate);
 	}
 
 	@Override
 	public TeacherCertificate queryTeacherCertificateById(Map<String, Object> map) {
-		return this.getSqlSession().selectOne("teacher.queryTeacherCertificateById", map);
+		return this.getSqlSession().selectOne("teacherCertificate.queryTeacherCertificateById", map);
 	}
 
 }
