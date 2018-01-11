5ed1f2d0a46dc414b0be0b93f3c4c88036d4713b
wangshengli <wangshengli@gomeholdings.com>
2018-01-11 15:25:23 +0800

    fix:清理架构测试代码

--- education-framework-parent/education-framework-dao/src/main/java/com/education/framework/dao/user/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.education.framework.dao.user;
-
-import com.education.framework.model.user.UserInfo;
-
-public interface UserDao {
-	UserInfo getUserById(int id);
-}
--- education-framework-parent/education-framework-service-api/src/main/java/com/education/framework/service/user/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.education.framework.service.user;
-
-import com.education.framework.model.user.UserInfo;
-
-public interface UserService {
-	public UserInfo getUser(int id);
-}
